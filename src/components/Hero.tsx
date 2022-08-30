0cde50953ed10c0705fabaefa054316b09d7f5c4
basON <wch.basinski@gmail.com>
2022-08-30 16:47:52 +0200

    Add assets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import party from "../assets/18trimed.mov";
+import { contentfulClient } from "../config/contentful";
+import { useNavigate } from "react-router-dom";
 function Hero() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const execute = async () => {
+      try {
+        const res: any = await contentfulClient.getEntries({
+          content_type: "mainMovie",
+        });
+        //setStory(res);
+
+        console.log(res);
+      } catch (err) {
+        navigate("/not-found");
+        console.log(err);
+      }
+      // const res = await client.getEntries({ content_type: "story" });
+    };
+    execute();
+  }, []);
   return (
     <div className="w-full h-screen relative">
       <video
